a935980418d34b56e79fc0c6ac9d2a845e156d22
aa <you@example.com>
2020-11-06 20:01:52 +0800

    echostr

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -24,7 +24,8 @@ class TextController extends Controller
             //记录日志
             file_put_contents("wx_event,log",$xml_str);
             //把xml转换PHP数据
-            echo "";
+            echo "echostr";
+            die;
         }else{
            echo "";
         }
